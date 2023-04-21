24e1dba4e0dce6fa3ae47a79cab3f1867f902ac0
Paola Frailis <paola.frailis@libero.it>
2023-04-21 18:53:39 +0200

    aggiunta componente di avvenuta registrazione

---
+++ Angular/src/app/@shared/components/registration-success/registration-success.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'tnv-registration-success',
+  templateUrl: './registration-success.component.html',
+  styleUrls: ['./registration-success.component.scss']
+})
+export class RegistrationSuccessComponent {
+
+}
