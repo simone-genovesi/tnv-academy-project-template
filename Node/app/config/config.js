e8a27eff90d8ed6bab7d4c8316ff6825ad2ba0c4
Simone Genovesi <smn.gnv92@gmail.com>
2023-04-27 11:30:35 +0200

    Undo PR

--- Node/app/config/config.js
+++ Node/app/config/config.js
@@ -3,7 +3,7 @@ import { Sequelize } from "sequelize";
 const db = new Sequelize({
   database: "tnv_final_project",
   username: "root",
-  password: "Lord1234-",
+  password: "Freedommini97",
   host: "localhost",
   port: 3306,
   dialect: "mysql",
