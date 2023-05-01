1d10cb795103afe8bf5a51626c99942eac73c0de
DrFraus <quikalvin@gmail.com>
2023-05-01 15:24:40 +0200

    modifica favorites

--- Node/app/src/models/favorite.js
+++ Node/app/src/models/favorite.js
@@ -7,6 +7,9 @@ const favoriteMovie = db.define('favorites', {
   userId: {
     type: DataTypes.INTEGER
   },
+  movieId: {
+    type: DataTypes.INTEGER
+  },
   movieTitle: {
     type: DataTypes.STRING
   },
